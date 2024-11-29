f27f2517de78e68d027e0a6acb39fdf78f7cf3e0
MANISHGHGH <95271062+MANISHGHGH@users.noreply.github.com>
2024-11-29 10:14:46 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to Manish Bhadauria's Website</h1>
+        <p>This is a basic website created using React.js.</p>
+        <p>Explore React.js and deploy amazing apps online using Vercel!</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
